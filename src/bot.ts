41d2884e3d6eb00cf2338028f1a4ef25af0a4487
ArsyadCyber <124241369+ArsyadCyber@users.noreply.github.com>
2024-04-18 06:04:49 +0000

    Add jest test and delete husky

--- src/bot.ts
+++ src/bot.ts
@@ -1,17 +1,19 @@
 // Import library Grammy
-require('dotenv').config();
-import { Bot, Context } from "grammy";
-import { Router } from "@grammyjs/router";
-import { startCommand } from "./commands/start";
+import dotenv from 'dotenv';
+dotenv.config();
+import {Bot, Context} from 'grammy';
+import {Router} from '@grammyjs/router';
+import {autoQuote} from '@roziscoding/grammy-autoquote';
+import {startCommand} from './commands/start';
 
 // Ganti "YOUR_BOT_TOKEN" dengan token bot Telegram Anda
 const bot = new Bot(process.env.BOT_TOKEN as string);
-const router = new Router((ctx: Context) => ctx.message?.text?.split(" ")[0]);
-
+const router = new Router((ctx: Context) => ctx.message?.text?.split(' ')[0]);
+bot.use(autoQuote());
 // Menangani pesan teks
-router.route("/start", startCommand);
+router.route('/start', startCommand);
 bot.use(router);
 // Mulai bot
 bot.start();
 
-console.log("Bot Telegram telah dimulai!");
+console.log('Bot Telegram telah dimulai!');
--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,5 +1,29 @@
-import { Context } from "grammy";
+import {Context} from 'grammy';
 
-export async function startCommand(ctx: Context) {
-  await ctx.reply("Selamat datang! Saya adalah bot sederhana.");
+export async function startCommand(ctx: Context): Promise<void> {
+  const fullName = ctx.from?.first_name + ' ' + ctx.from?.last_name;
+  const username = ctx.from?.username;
+  let currentHour = new Date().getUTCHours() + 7;
+
+  // Jika jam melebihi 24, kurangi 24 untuk mendapatkan jam dalam rentang 0-23
+  if (currentHour > 24) {
+    currentHour -= 24;
+  }
+  // Menentukan pesan selamat berdasarkan jam
+  let greeting;
+  if (currentHour >= 1 && currentHour < 10) {
+    greeting = 'Selamat pagi';
+  } else if (currentHour >= 10 && currentHour < 15) {
+    greeting = 'Selamat siang';
+  } else if (currentHour >= 15 && currentHour < 18) {
+    greeting = 'Selamat sore';
+  } else {
+    greeting = 'Selamat malam';
+  }
+  await ctx.reply(
+    `${greeting} *${fullName}* (_@${username}_), bot ini menyediakan fitur AI untuk kamu gunakan, silahkan gunakan _/help_ untuk melihat menu bantuan`,
+    {
+      parse_mode: 'Markdown',
+    }
+  );
 }
